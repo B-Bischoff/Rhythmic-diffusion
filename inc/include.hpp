fd90254d677096587493b63874b1bd8597e866c5
B-Bischoff <mainbischoffbrice@gmail.com>
2023-01-14 16:29:10 -0500

    putted audio playing in a thread + graph visu on UI

--- inc/include.hpp
+++ inc/include.hpp
@@ -14,9 +14,20 @@
 #include "../src/vendor/imgui/imgui_impl_glfw.h"
 #include "../src/vendor/imgui/imgui_impl_opengl3.h"
 
+#include "../external/kissfft/kiss_fftr.h"
+#include "../external/kissfft/kiss_fft.h"
+
+#include "../src/vendor/stk/FileWvIn.h"
+#include "../src/vendor/stk/RtAudio.h"
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
 #include <exception>
 #include <cmath>
+#include <complex>
+#include <cstdlib>
+#include <signal.h>
+
+#include <thread>
